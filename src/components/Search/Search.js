4030aca2d0a563a0779fec41db80dc9234eecc01
Oliver Evans <ollie@shufflebeedigital.co.uk>
2020-02-18 22:14:09 +0000

    Initial search markup component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import clsx from 'clsx';
 import './Search.scss';
 
 const Search = ({
-  cities
-}) => (
+  cities,
+  filterCities,
+}) => {
+  return (
   <>
-    <h1>Search</h1>
-    <ul>
-      {cities.map((city) => (
-        <li>{city.name}</li>
-      ))}
-    </ul>
+    <div className={clsx({
+      "search__wrapper": true,
+      "search__wrapper--open": true,
+    })}>
+      <div className="input__wrapper">
+        <input
+          type="search"
+          className="input"
+          placeholder="Enter city name..."
+          onChange={filterCities}
+        />
+      </div>
+      <ul className="search__results">
+        {cities.map((city) => (
+          <li key={city.name}>
+            <button>{city.name}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   </>
-);
+  );
+};
 
 export default Search;
